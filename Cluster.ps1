6eaad56863c47ae0435aa33a15877b889f2d9d1a
admnew <admnew@dipacademy.local>
2023-03-10 01:36:59 +0300

    Add Test and Creation

--- Cluster.ps1
+++ Cluster.ps1
@@ -1,6 +1,14 @@
-$Servers = "node1","node2"
+$node1 = "node1"
+$node2 = "node2"
+$Servers = $node1, $node2
 $ServerRoles = "Data-Center-Bridging","Failover-Clustering","Hyper-V","RSAT-Clustering-PowerShell","Hyper-V-PowerShell","FS-FileServer"
+$ClusterName =
+$ClusterIP =
 
 foreach ($server in $servers){
     Install-WindowsFeature –Computername $server –Name $ServerRoles} -Restart
 
+Test-Cluster –Node $node1,$node" –Include "Storage Spaces Direct", "Inventory", "Network", "System Configuration"
+
+
+New-Cluster –Name $ClusterName –Node "node1","node2" –NoStorage -StaticAddress $ClusterIP 
